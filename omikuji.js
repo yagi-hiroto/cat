de4b7b0b67e7eac4bb1c21fc6ae0c16a99ef14c1
yagi-hiroto <139330114+yagi-hiroto@users.noreply.github.com>
2023-07-15 00:13:00 +0900

    Update omikuji.js

--- omikuji.js
+++ omikuji.js
@@ -1,6 +1,9 @@
 let username;
 let userresult;
 username = prompt("お名前を教えて下さい。");
+if (username == "") {
+  username = "名無し";
+}
 document.getElementById("name").innerHTML = username;
 
 let rand = Math.floor( Math.random() * 5) ;
